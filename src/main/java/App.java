e3d8f04cd5481e4e4a56eda363f1738f7db9eed6
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-23 10:33:00 +0300

    imported required packages in app.java class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+
 public class App {
 }
