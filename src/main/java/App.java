7752160dbfe0a2d2bacf1f4bcf3329db1b039d56
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-23 12:08:24 +0300

    added stylist form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,10 +17,33 @@ public class App {
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+
+
         get("/stylists", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("stylists", Stylist.all());
+//            model.put("stylists", Stylist.all());
             model.put("template", "templates/stylists.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+        post("/stylists", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            String name = request.queryParams("name");
+            String gender = request.queryParams("gender");
+            String contact = request.queryParams("contact");
+            Stylist newStylist = new Stylist(name, gender, contact);
+            newStylist.save();
+            model.put("template", "templates/stylists.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+
+
+
+        get("/stylists/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/stylist_form.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
     }
