84ee8a48f30422811a2e4f6b4dd3b171f04aff85
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-23 07:36:35 +0300

    first testto test if saving client works

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
 public class Client {
+
+    private int id;
+    private String name;
+    private String gender;
+    private String contact;
+    private  int stylist_id;
+
+  public Client(String name, String gender,String contact, int stylist_id);{
+      this.name = name;
+      this.gender = gender;
+      this.contact = contact ;
+      this.stylist_id = stylist_id;
+    }
+
+    public int getId(){
+      return id;
+    }
+    public String getName(){
+      return name;
+    }
+    public String getGender(){
+      return gender;
+    }
+    public String getContact(){
+      return contact;
+    }
+
+
+    //method to return stylist Id
+    public Stylist getStylist(){
+      int id_check = stylist_id;
+      Stylist stylist;
+      if(id_check=0){
+          stylist - new Stylist("","","");
+      }else {
+          try(Connection con = DB.sql2o.open()){
+              String sql = "SELECT * FROM stylists where id=:id";
+              stylist = con.createQuery(sql)
+                      .addParameter("id",id)
+                      .executeAndFET
+          }
+      }
+
+    }
+
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,2 +1,24 @@
+
+import org.junit.Rule;
+import org.junit.*;
+import org.sql2o.*;
+import static org.junit.Assert.*;
+
+
 public class ClientTest {
+    @Test
+    public void savingClient_true(){
+        Client client = new Client("Yasmin","F","254",1);
+        client.save();
+        assertEquals("Yasmin",client.getName());
+    }
+    //test to find client list
+    @Test
+    pulic void  findClient_true(){
+        Client client = new Client("Yasmin","F","254",1);
+        client.save();
+        assertEquals("Yasmin", client.find(client.getId()).getName();
+    }
+    //test to update client list
 }
+
