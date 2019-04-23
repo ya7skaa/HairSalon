4617ba9e1909680b12dbe2a38b284815facccc55
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-23 07:45:16 +0300

    written test to updatew client list

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -14,11 +14,21 @@ public class ClientTest {
     }
     //test to find client list
     @Test
-    pulic void  findClient_true(){
+    public void  findClient_true(){
         Client client = new Client("Yasmin","F","254",1);
         client.save();
         assertEquals("Yasmin", client.find(client.getId()).getName();
     }
     //test to update client list
+    @Test
+    public void updatingClient_true() {
+        Client client = new Client("Yasmin","F","254",1);
+        client.save();
+        client.update("Abdi", "M", "254", 1);
+        assertEquals("Abdi", Client.find(client.getId()).getName());
+        assertEquals("M", Client.find(client.getId()).getGender());
+        assertEquals("254", Client.find(client.getId()).getContact());
+        assertEquals(2, Client.find(client.getId()).getStylist());
+    }
 }
 
