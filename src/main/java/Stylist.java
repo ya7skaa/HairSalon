7d3fe8ead60aa6ba33f022b5c4610232fda03b20
Abdirahman Osman <abdirahmanosman@Abdirahmans-MacBook-Pro.local>
2019-04-23 09:58:59 +0300

    testing the update methode

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -12,4 +12,83 @@ public class Stylist {
         this.gender = gender;
         this.contact = contact;
     }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public String getContact(){
+        return contact;
+    }
+    //method to return a Stylist object.
+    public static Stylist find(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM stylists where id=:id";
+            Stylist stylist = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Stylist.class);
+            return stylist;
+        }
+    }
+    // method to return all Stylist information from our stylist database table in the all() method:
+    public static List<Stylist> all() {
+        String sql = "SELECT id, name, gender, contact FROM stylists ORDER BY name";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Stylist.class);
+        }
+    }
+    // method for saving new object to the database && assign the object the same id as its data in the database:
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO stylists(name, gender, contact) VALUES (:name, :gender, :contact)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .addParameter("gender", this.gender)
+                    .addParameter("contact", this.contact)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    // method for getting clients from stylist object
+    public List<Client> getClients() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where stylist_id=:id ORDER BY name";
+            return con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeAndFetch(Client.class);
+        }
+    }
+    //method for updating the stylist object
+    public void update(String name, String gender, String contact) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE stylists SET name = :name, gender = :gender, contact = :contact WHERE id = :id";
+            con.createQuery(sql)
+                    .addParameter("name", name)
+                    .addParameter("gender", gender)
+                    .addParameter("contact", contact)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+    //deleting stylist object
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM stylists WHERE id = :id;";
+            con.createQuery(sql).addParameter("id", id).executeUpdate();
+        }
+        //Assign client_id to 0 for Clients allocated to the deleted Stylist
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "UPDATE clients SET stylist_id = 0 WHERE stylist_id = :id;";
+            con.createQuery(sql).addParameter("id", id).executeUpdate();
+        }
+    }
+
 }
+
+
+
+
--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -1,2 +1,29 @@
+import org.junit.Rule;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
 public class StylistTest {
+
+    @Before
+    public void setUp() {
+        DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+    }
+
+    @After
+    public void tearDown() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM clients *;";
+            con.createQuery(sql).executeUpdate();
+        }
+    }
+
+    // testing save method
+    @Test
+    public void savingStylist_true() {
+        Stylist stylist = new Stylist("Alpha","M","500");
+        stylist.save();
+        assertEquals("Alpha", stylist.getName());
+    }
 }
